467d972775c033ced81560f8d89bdf3e58313d75
hskimstudy <hskimstudy@gmail.com>
2022-08-10 14:11:50 +0900

    button change

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark')
+  }
+  else {
+    navbar.classList.remove('navbar--dark')
+  }
+});
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link)
+  const scrollTo = document.querySelector(link)
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+})
